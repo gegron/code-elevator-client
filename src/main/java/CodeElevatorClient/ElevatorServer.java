d976cb2de63b601e93bd40833da2f69e70123e2d
Gerome Egron <legunda.gg&gmail.com>
2013-09-28 23:07:10 +0200

    First commit

---
+++ src/main/java/CodeElevatorClient/ElevatorServer.java
@@ -0,0 +1,24 @@
+package CodeElevatorClient;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.get;
+import static spark.Spark.setPort;
+
+public class ElevatorServer {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        setPort(PORT);
+
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+    }
+
+}
