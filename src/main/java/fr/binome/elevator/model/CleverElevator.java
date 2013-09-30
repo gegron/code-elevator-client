189e75a9c2add9c7ffb2dcef75f5e0c1a97ed183
MathieuLoiselle <mathieu.loiselle.2010@gmail.com>
2013-09-30 22:31:51 +0200

    First commit with IntelliJ !!!

--- src/main/java/fr/binome/elevator/model/CleverElevator.java
+++ src/main/java/fr/binome/elevator/model/CleverElevator.java
@@ -9,22 +9,50 @@ import static fr.binome.elevator.model.ElevatorResponse.*;
 
 public class CleverElevator extends Elevator {
 
+    /** TODO: Rethink because of elevator empty + person calls at the current floor **/
     private boolean doorsAlreadyOpenAtThisLevel = false;
 
-    private Map<Integer, Boolean> callRecorder = new HashMap<Integer, Boolean>() {{
+    private Map<Integer, Boolean> destinations = new HashMap<Integer, Boolean>() {{
         for (int i = MIN_LEVEL; i <= MAX_LEVEL; i++) {
             put(i, false);
         }
     }};
 
-    @Override
-    public void go(Integer floorToGo) {
-        callRecorder.put(floorToGo, true);
+    private Map<Integer, Boolean> callsUp = new HashMap<Integer, Boolean>() {{
+        for (int i = MIN_LEVEL; i <= MAX_LEVEL; i++) {
+            put(i, false);
+        }
+    }};
+
+    private Map<Integer, Boolean> callsDown = new HashMap<Integer, Boolean>() {{
+        for (int i = MIN_LEVEL; i <= MAX_LEVEL; i++) {
+            put(i, false);
+        }
+    }};
+
+    private Map<Integer, Boolean> getCalls(String inWay) {
+        if (ElevatorResponse.valueOf(inWay) == UP) {
+            return callsUp;
+        }
+        if (ElevatorResponse.valueOf(inWay) == DOWN) {
+            return callsDown;
+        }
+
+        return null;
+    }
+
+    private Map<Integer, Boolean> getCalls(ElevatorResponse inWay) {
+        return getCalls(inWay.name());
     }
 
     @Override
-    public void call(Integer atFloor, String way) {
-        callRecorder.put(atFloor, true);
+    public void go(Integer floorToGo) {
+        destinations.put(floorToGo, true);
+    }
+
+    @Override
+    public void call(Integer atFloor, String inWay) {
+        getCalls(inWay).put(atFloor, true);
     }
 
     @Override
@@ -43,7 +71,7 @@ public class CleverElevator extends Elevator {
 
     @VisibleForTesting
     boolean doorsMustOpenAtThisLevel() {
-        return callRecorder.get(currentLevel);
+        return (destinations.get(currentLevel) || getCalls(way).get(currentLevel));
     }
 
     @VisibleForTesting
@@ -70,7 +98,8 @@ public class CleverElevator extends Elevator {
     private ElevatorResponse openDoor() {
         doorsAlreadyOpenAtThisLevel = true;
 
-        callRecorder.put(currentLevel, false);
+        destinations.put(currentLevel, false);
+        getCalls(way).put(currentLevel, false);
 
         return stateDoors = OPEN;
     }
